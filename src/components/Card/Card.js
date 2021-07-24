82f5aa496bd25a27b2596b8f11d0d087aa02a05c
kshivam99 <sawarn101@gmail.com>
2021-07-25 01:50:18 +0530

    highlight

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,12 +12,27 @@ function Card({ id, name, items, address, pincode, isHovered, keyboard }) {
           ref.current.scrollIntoView();
       }
   })
+
+  function formatText(input, pattern){
+    let temp = input.split(pattern);
+    let output;
+    output = temp.map((item, index)=>{
+        if(index===temp.length-1){
+            return item;
+        }
+        else{
+            return item +`<span style="color:#3B82F6; font-weight:800">${pattern}</span>`;
+        }
+    })
+    return output.join("");
+  }
+
   return (
     <div className={keyboard?isHovered?`${styles.card1} ${styles.card1hover}`:styles.card1:styles.card0} ref={ref}>
       <h4 className={styles.id}>{id}</h4>
-      <p className={styles.name}>{name}</p>
+      <p className={styles.name} dangerouslySetInnerHTML={{__html: formatText(name,pattern)}}/>
       {JSON.stringify(items).toUpperCase().includes(pattern.toUpperCase()) && (
-        <p><strong>"{pattern}"</strong> found in items</p>
+        <p><span className={styles.dot}></span><strong>"{pattern}"</strong> found in items</p>
       )}
       <p>{address}</p>
       <small>{pincode}</small>
